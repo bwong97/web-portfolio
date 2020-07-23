e67dffbb4e5112d4fe68f53dea087fa9e6218a7c
bwong97 <bwong237@my.bcit.ca>
2020-07-22 23:11:17 -0700

    updated favicon, changed styling, moved components around

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,36 @@
 import React from 'react'
 
+import SoundQA from '../Projects/portfolio/SoundQA'
+import VoiceFirst from '../Projects/portfolio/VoiceFirst'
+import EarlsKitchen from '../Projects/portfolio/EarlsKitchen'
+
 function Home() {
   return (
     <div className="App-header">
-      <h1>&nbsp;</h1>
-      <h1 className="My-name">Bradley Wong</h1>
-      <h2>Front End Web Developer</h2>
-      <p>Diploma Graduate from BCIT 2020 in Computer Information Technology</p>
-      <h5>~ Webapp under construction ~</h5>
+      <div className="home-top">
+        <h1 className="My-name">Bradley Wong</h1>
+        <h2>Front End Web Developer</h2>
+        <p>Diploma Graduate from BCIT 2020 in Computer Information Technology</p>
+      </div>
+      <section className="home-about">
+        <h1>TL;DR</h1>
+        <br/>
+        <p className="about-blurb">I am a recent graduate from BCIT's Computer Information Technology Diploma Program.
+          I have covered mobile/web development in React, scripting in Python and Bash, automation
+          using tools such as Ansible, and also some knowledge of deploying applications to cloud platforms
+          such as AWS and Azure.</p>
+      </section>
+      <section className="home-projects">
+        <h1>My Projects</h1>
+        <br/>
+        <EarlsKitchen />
+        <br/>
+        <SoundQA />
+        <br/>
+        <VoiceFirst />
+        <br/>
+        <br/>
+      </section>
     </div>
   )
 }
